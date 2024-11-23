f2c2c5f03e9b434ebdc98b21d70e7f18e1912723
Adonis <adonis.santos02@icloud.com>
2024-11-23 14:45:05 -0500

    Create Section class with two public functions

---
+++ components/Section.js
@@ -0,0 +1,16 @@
+class Section {
+  constructor({ renderer }, selector) {
+    this._renderer = renderer;
+    this._element = document.querySelector(`.${selector}`);
+  }
+
+  renderItems(items) {
+    items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._element.append(element);
+  }
+}
