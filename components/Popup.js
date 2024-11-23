44561cf9d21ee1d9ab81fa1f34e4e3070c26802b
Adonis <adonis.santos02@icloud.com>
2024-11-23 15:26:07 -0500

    Create Popup class. Refactor  open, close, esc, and mouseclick functions from index.js into Popup class

---
+++ components/Popup.js
@@ -0,0 +1,33 @@
+class Popup {
+  constructor(popupselector) {
+    this._selector = document.querySelector(`#${popupselector}`);
+  }
+
+  open() {
+    this._selector.classList.add("modal_opened");
+    document.addEventListener("keyup", this._handleEscClose);
+  }
+
+  close() {
+    this._selector.classList.remove("modal_opened");
+    document.removeEventListener("keyup", this._handleEscClose);
+  }
+
+  _handleEscClose(e) {
+    e.preventDefault();
+    if (e.key === "Escape") {
+      this.close();
+    }
+  }
+
+  _setEventListeners() {
+    this._selector.addEventListener("mousedown", (e) => {
+      if (
+        e.target.classList.contains("modal") ||
+        e.target.classlist.contains("modal__close")
+      ) {
+        this.close();
+      }
+    });
+  }
+}
