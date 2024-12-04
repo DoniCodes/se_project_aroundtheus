17902d59ff320b2723582ceebcdd74e8327ca4ab
Adonis <adonis.santos02@icloud.com>
2024-12-04 16:21:53 -0500

    Refactor constructor and private method

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,29 +1,32 @@
 import Popup from "./Popup";
 
 class PopupWithForm extends Popup {
-  constructor({ popupSelector }, handleFormSubmit) {
-    super({ popupSelector });
+  constructor({ popupSelector, handleFormSubmit }) {
+    super(popupSelector);
 
     this._popupForm = this._selector.querySelector(".modal__form");
-    this._inputList = this._selector.querySelectorAll(".modal__input");
+
     this._handleFormSubmit = handleFormSubmit;
   }
 
   _getInputValues() {
-    const formValues = {};
+    this._inputList = this._selector.querySelectorAll(".modal__input");
+
+    this._formValues = {};
     this._inputList.forEach((input) => {
-      formValues[input.name] = input.value;
+      this._formValues[input.name] = input.value;
     });
-    return formValues;
+    return this._formValues;
   }
 
   setEventListeners() {
-    super.setEventListeners();
     this._selector.addEventListener("submit ", (e) => {
       e.preventDefault();
-      this._handleFormSubmit(this._getInputValues);
+      this._handleFormSubmit(this._getInputValues());
       this.close();
     });
+
+    super.setEventListeners();
   }
 
   close() {
