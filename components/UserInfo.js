ff0943f22cd76c62c2e5365c4943b586543e6005
Adonis <adonis.santos02@icloud.com>
2024-12-14 11:40:48 -0500

    Add UserInfo class with constructor and two public methods

---
+++ components/UserInfo.js
@@ -0,0 +1,20 @@
+class UserInfo {
+  constructor({ userName, userDescription }) {
+    this._userName = document.querySelector(`.${userName}`);
+    this._userDescription = document.querySelector(`.${userDescription}`);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._userName.textContent,
+      description: this._userDescription.textContent,
+    };
+  }
+
+  setUserInfo({ name, description }) {
+    this._userName.textContent = name;
+    this._userDescription.textContent = description;
+  }
+}
+
+export default UserInfo;
