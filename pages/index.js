52825ced30cdfb46d06df669c1b3370eef7fd61a
Adonis <adonis.santos02@icloud.com>
2024-12-14 11:45:28 -0500

    Add UserInfo and PopupWithForm logic. Remove commented code

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,43 @@ import FormValidator from "../components/FormValidator.js";
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
 
-const profileEditForm = document.forms["profile-form"];
-const addCardForm = document.forms["card-form"];
+const userInfo = new UserInfo({
+  userName: selectors.profileTitle,
+  userDescription: selectors.profileDescription,
+});
 
 const popupWithImage = new PopupWithImage({
   popupSelector: selectors.previewPopup,
+});
+
+const userInfoPopup = new PopupWithForm({
+  popupSelector: selectors.profileEditForm,
+  handleFormSubmit: (data) => {
+    userInfo.setUserInfo({
+      name: data.profileTitle,
+      description: data.profileDescription,
+    });
+    userInfoPopup.close();
+  },
+});
+
+const newCardPopUp = new PopupWithForm({
+  popupSelector: selectors.addCardForm,
+  handleFormSubmit: (cardData) => {
+    const card = new Card(
+      {
+        data: cardData,
+        handleImageClick: () => {
+          popupWithImage.open(cardData);
+        },
+      },
+      selectors.cardTemplate
+    );
+    cardSection.addItem(card.generateCard());
+  },
 });
 
 const cardSection = new Section(
@@ -32,89 +63,21 @@ const cardSection = new Section(
   },
   selectors.cardList
 );
+userInfoPopup.setEventListeners();
+newCardPopUp.setEventListeners();
 popupWithImage.setEventListeners();
 cardSection.renderItems(initialCards);
 
 //Validation
 
-const addFormValidator = new FormValidator(formValidationConfig, addCardForm);
+const addFormValidator = new FormValidator(
+  formValidationConfig,
+  selectors.addCardForm
+);
 addFormValidator.enableValidation();
 
 const editFormValidator = new FormValidator(
   formValidationConfig,
-  profileEditForm
+  selectors.profileEditForm
 );
 editFormValidator.enableValidation();
-
-/* const closeModal = (modal) => {
-  modal.classList.remove("modal_opened");
-  document.removeEventListener("keyup", handleEscUp);
-  document.removeEventListener("mouseup", handleOverlayClick);
-};
-
-const openModal = (modal) => {
-  modal.classList.add("modal_opened");
-  document.addEventListener("keyup", handleEscUp);
-  document.addEventListener("mouseup", handleOverlayClick);
-};
-
-const isEscEvent = (e, action) => {
-  if (e.key === "Escape") {
-    const activeModal = document.querySelector(".modal_opened");
-    action(activeModal);
-  }
-};
-
-const handleEscUp = (e) => {
-  e.preventDefault();
-  isEscEvent(e, closeModal);
-};
-
-const handleOverlayClick = (e) => {
-  if (e.target.classList.contains("modal_opened")) {
-    closeModal(e.target);
-  }
-}; */
-
-/* const handleProfileSubmit = (e) => {
-  e.preventDefault();
-  profileTitle.textContent = profileTitleInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
-  closeModal(profileEditModal);
-}; */
-
-/* const handleAddCardSubmit = (e) => {
-  e.preventDefault();
-  const name = addCardTitleInput.value;
-  const link = addCardUrlInput.value;
-  renderCard({ name, link }, cardListEl);
-  closeModal(addCardModal);
-  e.target.reset();
-  addFormValidator.toggleButtonState();
-}; */
-
-/* const handleImageClick = (name, link) => {
-  previewModalImg.src = link;
-  previewModalImg.alt = name;
-  previewModalCaption.textContent = name;
-  openModal(previewModal);
-}; */
-
-/* modalCloseButtons.forEach((modalCloseBtn) => {
-  modalCloseBtn.addEventListener("click", (e) => {
-    const modals = modalCloseBtn.closest(".modal");
-    closeModal(modals);
-  });
-});
-
-profileEditButton.addEventListener("click", () => {
-  profileTitleInput.value = profileTitle.textContent;
-  profileDescriptionInput.value = profileDescription.textContent;
-  openModal(profileEditModal);
-});
-
-profileEditForm.addEventListener("submit", handleProfileSubmit);
-
-addCardButton.addEventListener("click", () => {
-  openModal(addCardModal);
-}); */
